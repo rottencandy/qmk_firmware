86d44d0cf51801930e72e59b23cad5ef28be93dc
saud <md.saud020@gmail.com>
2024-03-08 19:05:05 +0530

    add my config

---
+++ keyboards/sofle/keymaps/rottencandy/rules.mk
@@ -0,0 +1,12 @@
+TRI_LAYER_ENABLE = yes
+
+BOOTLOADER = caterina
+# to get .uf2 file
+CONVERT_TO=promicro_rp2040
+
+VIA_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+ENCODER_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
