020581214dc6f532390a584c67ddefe63424912d
saud <md.saud020@gmail.com>
2024-09-21 23:16:08 +0530

    add custom oled output

--- keyboards/crkbd/keymaps/rottencandy/rules.mk
+++ keyboards/crkbd/keymaps/rottencandy/rules.mk
@@ -1,4 +1,6 @@
-MOUSEKEY_ENABLE = no     # Mouse keys
-VIA_ENABLE      = yes    # Enable VIA
-LTO_ENABLE      = yes
-CONVERT_TO      = kb2040
+MOUSEKEY_ENABLE  = no     # Mouse keys
+VIA_ENABLE       = yes    # Enable VIA
+LTO_ENABLE       = yes
+CONVERT_TO       = kb2040
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
